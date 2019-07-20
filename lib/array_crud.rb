515aa78036f8ff5356106a15f943947de4082cd3
Warren Loy Harper II <warrenharper2@att.net>
2019-07-20 17:12:05 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+ empty_array = [] 
 end
 
 def create_an_array
-  
+ harper_family = ["warren","shirley","loy","sabrina"] 
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  harper_family <<"kelsi"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  harper_family.push(kimberly)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  kelisi_harper = harper_family.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  warren_harper = harper_family.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  harper_family[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  harper_family[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  harper_family[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+harper_family [2] = "kristopher"
 end
